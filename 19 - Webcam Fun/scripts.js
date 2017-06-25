b34509c39ae527f7c4062fe811bc5c1761a9bd9d
Rohit Rai <rohitkrai03@gmail.com>
2017-06-25 23:03:23 +0530

    working webcam feed and taking snaps feature

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,45 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch( err => {
+      console.error('You didnt allow Webcam access.', err);
+    });
+}
+
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto(){
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src=${data} alt='handsome man' />`;
+
+  strip.insertBefore(link, strip.firstChild);
+}
+
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
